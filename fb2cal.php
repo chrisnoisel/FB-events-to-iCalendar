1ba0a678286c9a242e7a3929e09e642856501b67
Christophe Noisel <christophe@fidzup.com>
2014-04-27 20:43:14 +0200

    added GET parameters (page-id / event-id)

--- fb2cal.php
+++ fb2cal.php
@@ -1,44 +1,85 @@
 <?php
-
 require('fb2cal.conf.php'); // contains access token ($token)
-$id = "369191986522047"; // Facebook group ID
+$id = "369191986522047"; // Default Facebook group ID
+
+//// log requests (usefull to see when Google refreshes google calendar : each 6-8 hours)
+//$date = date(DATE_RFC2822);
+//file_put_contents("fb2cal.log.txt", $date."  ".$_SERVER['REMOTE_ADDR']."  ".$_SERVER['HTTP_USER_AGENT'].PHP_EOL, FILE_APPEND);
+////
 
 ///////////////////////////////////////////
 
-
-//header('Content-Type: text/html; charset=utf-8');
-header('Content-Type: text/calendar; charset=utf-8');
-header('Content-Disposition: attachment; filename=cal_'.$id.'.ics');
-
-$baseurl = "https://graph.facebook.com/";
-$params = "/events?fields=description,name,start_time,end_time,location,owner,id,venue,updated_time";
-$endurl = "&method=GET&format=json&suppress_http_code=1&access_token=";
-$appendurl = "https://www.facebook.com/events/";
-
-$json = json_decode(file_get_contents($baseurl.$id.$params.$endurl.$token), true);
-$data = $json['data'];
-
-?>
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Facebook//NONSGML Facebook Events V1.0//EN
-<?php
-
-function formatDate($fbDate) {
-	$time = strtotime($fbDate);
-	return gmdate('Ymd\THis\Z', $time);
+function setHTTPHeaders($text_only) {
+	if ($text_only)
+		header('Content-Type: text/html; charset=utf-8');
+	else {
+		header('Content-Type: text/calendar; charset=utf-8');
+		header('Content-Disposition: attachment; filename=cal_'.$id.'.ics');
+	}
 }
 
-foreach($data as $val) {
+function requestOwner($id, $token) {
+	$url = "https://graph.facebook.com/[id]?method=GET&format=json&suppress_http_code=1&access_token=[token]";
+	$url = str_replace("[id]", $id, $url);
+	$url = str_replace("[token]", $token, $url);
+	$json = json_decode(file_get_contents($url), true);
+	return $json['name'];
+}
+
+function requestEvents($id, $token) {
+	$fields = "description,name,start_time,end_time,location,owner,id,venue,updated_time";
+	$url = "https://graph.facebook.com/[id]/events?fields=[fields]&method=GET&format=json&suppress_http_code=1&access_token=[token]";
+  $url = str_replace("[id]", $id, $url);
+	$url = str_replace("[token]", $token, $url);
+	$url = str_replace("[fields]", $fields, $url);
+
+	$json = json_decode(file_get_contents($url), true);
+	return $json['data'];
+}
+
+function requestEvent($id, $token) {
+	$fields = "description,name,start_time,end_time,location,owner,id,venue,updated_time";
+	$url = "https://graph.facebook.com/[id]?fields=[fields]&method=GET&format=json&suppress_http_code=1&access_token=[token]";
+	$url = str_replace("[id]", $id, $url);
+	$url = str_replace("[token]", $token, $url);
+	$url = str_replace("[fields]", $fields, $url);
+
+	return json_decode(file_get_contents($url), true);
+}
+
+function getVCALHeader($name = NULL) {
+	$vcard = "BEGIN:VCALENDAR".chr(10);
+	$vcard .= "VERSION:2.0".chr(10);
+	$vcard .= "PRODID:-//Facebook//NONSGML Facebook Events V1.0//EN".chr(10);
+	
+	if(isset($name) && !empty($name))
+		$vcard .= "X-WR-CALNAME:".$name." - Facebook".chr(10);
+	
+	return $vcard;
+}
+
+function formatDate($fieldName, $fbDate) {
+	$time = strtotime($fbDate);
+	
+	if (strstr($fbDate, "T") !== FALSE)
+		return $fieldName.":".gmdate('Ymd\THis\Z', $time);
+	else
+		return $fieldName.";VALUE=DATE:".date('Ymd', $time);
+}
+
+function print_event($val) {
+	$appendurl = "https://www.facebook.com/events/";
 
 	echo "BEGIN:VEVENT".chr(10);
 	echo "UID:e".$val['id']."@facebook.com".chr(10);
 	echo "SUMMARY:".$val['name'].chr(10);
 	echo "LOCATION:".$val['location'].chr(10);
 	
-	echo "LAST-MODIFIED:".formatDate($val['updated_time']).chr(10);
-	echo "DTSTART:".formatDate($val['start_time']).chr(10);
-	echo "DTEND:".formatDate($val['end_time']).chr(10);
+	echo formatDate("LAST-MODIFIED", $val['updated_time']).chr(10);
+	echo formatDate("DTSTART", $val['start_time']).chr(10);
+
+	if (isset($val['end_time']))
+		echo formatDate("DTEND", $val['end_time']).chr(10);
 
 	$description = str_replace(',', '\,', $val['description']);
 	$description = str_replace(chr(10), '\n', $description);
@@ -46,6 +87,30 @@ foreach($data as $val) {
 	echo "DESCRIPTION:".$description.chr(10);
 
 	echo "END:VEVENT".chr(10);
+}
+
+$baseurl = "https://graph.facebook.com/";
+$event_only = false;
+
+setHTTPHeaders(isset($_GET['text']));
+
+if (isset($_GET['event-id']) && is_numeric($_GET['event-id'])) {
+	echo getVCALHeader();
+	print_event(requestEvent($_GET['event-id'], $token));	
+}
+else {
+	if (isset($_GET['page-id']) && is_numeric($_GET['page-id']))
+		$id = $_GET['page-id'];
+	else if (isset($_GET['id']) && is_numeric($_GET['id']))
+		$id = $_GET['id']; 
+	
+	$name = requestOwner($id, $token);
+	echo getVCALHeader($name);
+	
+	$events = requestEvents($id, $token);
+	if (!empty($events)) foreach($events as $event) {
+		print_event($event);
+	}
 }
 
 ?>
